687d21d5d64d447c5ce444751b85e4fb1a10c90f
hanhan0901 <115163688+hanhan0901@users.noreply.github.com>
2022-10-07 11:46:36 +0800

    Add files via upload

---
+++ week5-4.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+
+    for(int i=0;i<5;i++){///電腦,精簡
+        printf("電腦從0開始數,i:%d\n",i);
+    }
+    for(int i=1;i<5;i++){///人類,多一點點
+        printf("人類從1開始數一點點,i:%d\n",i);
+    }
+
+}
