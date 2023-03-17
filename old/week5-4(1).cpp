6fd8900727e7f8aff5e978d1ab5734435b56f990
jsyeh <jsyeh@mail.mcu.edu.tw>
2023-03-17 11:43:59 +0800

    修改目錄、新增week05

---
+++ old/week5-4(1).cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+
+    for(int i=0;i<5;i++){///迴圈基本型,電腦,精簡
+        printf("電腦從0開始數,i:%d\n",i);
+    }
+    for(int i=1;i<5;i++){///迴圈進階型,人類,多一點點
+        printf("人類從1開始數一點點,i:%d\n",i);
+    }
+    for(int a=5;a>0;a--){///迴圈倒過來型
+        printf("倒過來的迴圈,a:%d\n",a);
+}
+}
